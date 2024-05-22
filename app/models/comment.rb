6fca6e4e61cb82d4b602287795d0601c26dcdfac
Carey Benge <csbenge@gmail.com>
2024-05-22 08:43:09 -0500

    first commit

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  include Visible
+
+  belongs_to :article
+
+  validates :commenter, presence: true, length: { minimum: 5 }
+  validates :body,  presence: true, length: { minimum: 10 }
+end
