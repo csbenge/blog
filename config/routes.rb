d7231a1de084d460ab48cf687539864692e44989
Carey Benge <csbenge@gmail.com>
2024-05-23 16:27:13 -0500

    added users

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
 
+  get 'signup'  => 'users#new'
+  resources :users
   resources :articles do
     resources :comments
   end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = Article.new(title: "Article TItle", body: "article body", status: "public")
+  end
+
+  test "should be valid" do
+    assert @article.valid?
+  end
+
+  test "title should be present" do
+    @article.title = "     "
+    assert_not @article.valid?
+  end
+
+  test "body should be present" do
+    @article.body = "     "
+    assert_not @article.valid?
+  end
+
+  test "status should be present" do
+    @article.status = "     "
+    assert_not @article.valid?
+  end
 end
