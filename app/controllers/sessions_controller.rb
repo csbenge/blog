62936a987ee632f3b7f0de500a5ef316338649ae
Carey Benge <csbenge@gmail.com>
2024-06-03 12:40:59 -0500

    Clean up previous merge and fix topnav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       flash.clear
+      flash[:success] = 'Welcome back to BLOGGER!'
       log_in user
       redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.clear
+      flash[:error] = 'Invalid email/password combination'
       render :new, status: :unprocessable_entity
     end
   end
