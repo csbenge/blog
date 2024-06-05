7b28e77659939b8bab177e247e931fa366554356
Carey Benge <csbenge@gmail.com>
2024-06-05 15:24:51 -0500

    Clean up views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+    
+    def getArticleUser(id)
+        user = User.find(id)
+        user
+    end
+    
+    def getArticleUserName(id)
+        user = User.find(id)
+        user.name
+    end
 end
